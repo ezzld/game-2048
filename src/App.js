055fbf4146227e6a897955122a21c6e6a0926b3d
ezzld <azoz.19972118@gmail.com>
2021-09-07 03:11:08 +0300

    edit box

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import GameController from "./GameController";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <GameController />
+    </div>
+  );
+}
+
+export default App;
